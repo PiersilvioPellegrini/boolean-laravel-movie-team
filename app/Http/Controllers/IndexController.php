12d9ff9f1084acc1b768358b58178af41ff64dba
Sofia-Citrini <sofiacitrini@gmail.com>
2023-01-25 17:10:41 +0100

    completed controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Movie;
 
 class IndexController extends Controller
 {
     function index(){
-        return view('index');
+
+        $allMovies = Movie::all();
+
+        return view('index', compact('allMovies'));
     }
 }
